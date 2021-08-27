26c24ec0c26736ab341d9eed7f9549bd92edd705
Ariel Weingarten <asweingarten@gmail.com>
2021-08-27 15:09:39 -0700

    Switch from PSM 7 to 8
    - comment out roi extraction

--- run_OCR.py
+++ run_OCR.py
@@ -2,11 +2,11 @@ import os
 
 # How to execute shell commands with Python https://janakiev.com/blog/python-shell-commands/
 rois = ['left', 'right', 'both']
-for r in rois:
-	os.system(f'rm kill-count-rois/{r}/*.png')
+# for r in rois:
+# 	os.system(f'rm kill-count-rois/{r}/*.png')
 
-os.system("conda run -n tesseract python3 watch_video.py")
-tesseract_command = f"tesseract imgs.txt results --psm 7 -c tessedit_char_whitelist=0123456789"
+# os.system("conda run -n tesseract python3 watch_video.py")
+tesseract_command = f"tesseract imgs.txt results --psm 8 -c tessedit_char_whitelist=0123456789"
 for r in rois:
 	os.system(f"ls kill-count-rois/{r} | grep png > kill-count-rois/{r}/imgs.txt")
 	if r != 'both':
