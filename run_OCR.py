2afb9f0ee6476dd426d5749e7580d4f059a1624e
Ariel Weingarten <asweingarten@gmail.com>
2021-08-27 12:28:36 -0700

    Don't run OCR on "both" rois

--- run_OCR.py
+++ run_OCR.py
@@ -6,4 +6,5 @@ tesseract_command = f"tesseract imgs.txt results --psm 7 -c tessedit_char_whitel
 rois = ['left', 'right', 'both']
 for r in rois:
 	os.system(f"ls kill-count-rois/{r} | grep png > kill-count-rois/{r}/imgs.txt")
-	os.system(f"cd kill-count-rois/{r} && {tesseract_command}")
+	if r != 'both':
+		os.system(f"cd kill-count-rois/{r} && {tesseract_command}")
