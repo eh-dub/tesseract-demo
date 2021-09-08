318c069694a01b7fae66d8c89b27c36503e91753
Ariel Weingarten <asweingarten@gmail.com>
2021-09-07 21:25:41 -0400

    me-specific additions

--- run_OCR.py
+++ run_OCR.py
@@ -1,12 +1,13 @@
 import os
 
 # How to execute shell commands with Python https://janakiev.com/blog/python-shell-commands/
-rois = ['left', 'right', 'both']
-# for r in rois:
-# 	os.system(f'rm kill-count-rois/{r}/*.png')
+# rois = ['left', 'right', 'both']
+rois = ['right']
+for r in rois:
+	os.system(f'rm kill-count-rois/{r}/*.png')
 
-# os.system("conda run -n tesseract python3 watch_video.py")
-tesseract_command = f"tesseract imgs.txt results --psm 8 -c tessedit_char_whitelist=0123456789"
+os.system("poetry python3 watch_video.py")
+tesseract_command = f"tesseract imgs.txt results --dpi 300 --psm 7 -c tessedit_char_whitelist=0123456789 pdf" # tessedit_write_images=true"
 for r in rois:
 	os.system(f"ls kill-count-rois/{r} | grep png > kill-count-rois/{r}/imgs.txt")
 	if r != 'both':
