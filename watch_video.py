d4393406e6ec60845afeb087036d0c9928a79e1b
Ariel Weingarten <asweingarten@gmail.com>
2021-09-07 22:03:29 -0400

    Adjust deps

--- watch_video.py
+++ watch_video.py
@@ -2,7 +2,7 @@
 import av
 from extractROIsFromVideo import extractROIsFromVideo
 
-video_path = "./kill-clip.mkp4"
+video_path = "./kill-clip.mp4"
 container = av.open(video_path)
 
 FPS = float(container.streams.video[0].average_rate)
@@ -12,7 +12,7 @@ bboxes = [(1212,30,1228,50)]
 [rois] = extractROIsFromVideo(container, bboxes, FPS, 0, 300)
 
 for roi in rois:
-    roi.save(f'./kill-count-rois/right/{roi.info["timestamp"]}.png')
+    roi.save(f'./kill-count-rois/{roi.info["timestamp"]}.png')
         
 exit(0)
 
