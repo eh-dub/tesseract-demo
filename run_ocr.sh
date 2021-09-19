6c7d08eed5bdb679835a4577e3a158badabb83c1
Ariel Weingarten <asweingarten@gmail.com>
2021-09-19 16:02:50 -0400

    Build docker container in script

--- run_ocr.sh
+++ run_ocr.sh
@@ -1,4 +1,5 @@
 rm kill-count-rois/*.png
+docker build -t tesseract-demo .
 docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo conda run -n tesseract python3 watch_video.py
 ls kill-count-rois/ | grep png > kill-count-rois/imgs.txt
 docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo bash -c "cd kill-count-rois && conda run -n \"tesseract\" tesseract imgs.txt results --psm 8 -c tessedit_char_whitelist=0123456789"
