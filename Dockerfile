0e5567c0a1208360989d4c319aee36f82930c681
Ariel Weingarten <asweingarten@gmail.com>
2021-09-19 15:31:43 -0400

    Switch back to conda and get run_ocr.sh working

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM python:3.9-slim
 
-RUN pip install --user poetry
-ENV PATH="${PATH}:/root/.local/bin"
+#Install MINICONDA
+RUN apt-get update && apt-get install -y wget && wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh && \
+    /bin/bash Miniconda.sh -b -p /opt/conda 
 
-WORKDIR /tesseract-demo
+ENV PATH /opt/conda/bin:$PATH
+
+WORKDIR /tesseract
+
+COPY environment.yml ./
+
+RUN conda env create -f environment.yml
--- run_ocr.sh
+++ run_ocr.sh
@@ -1,4 +1,4 @@
 rm kill-count-rois/*.png
-docker run tesseract-demo -v \"$(pwd)\"/:/tesseract-demo poetry run python3 watch_video.py
+docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo conda run -n tesseract python3 watch_video.py
 ls kill-count-rois/ | grep png > kill-count-rois/imgs.txt
-docker run tesseract-demo -v \"$(pwd)\"/:/tesseract-demo cd kill-count-rois && poetry run tesseract imgs.txt results --psm 8 -c tessedit_char_whitelist=0123456789
+docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo bash -c "cd kill-count-rois && conda run -n \"tesseract\" tesseract imgs.txt results --psm 8 -c tessedit_char_whitelist=0123456789"
