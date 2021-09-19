6c1e75ae26951edd7f70defdc23e442e48a988a4
Ariel Weingarten <asweingarten@gmail.com>
2021-09-19 16:18:55 -0400

    output results in tsv

--- run_ocr.sh
+++ run_ocr.sh
@@ -2,4 +2,4 @@ rm kill-count-rois/*.png
 docker build -t tesseract-demo .
 docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo conda run -n tesseract python3 watch_video.py
 ls kill-count-rois/ | grep png > kill-count-rois/imgs.txt
-docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo bash -c "cd kill-count-rois && conda run -n \"tesseract\" tesseract imgs.txt results --psm 8 -c tessedit_char_whitelist=0123456789"
+docker run -v "$(pwd)"/:/tesseract --rm tesseract-demo bash -c "cd kill-count-rois && conda run -n \"tesseract\" tesseract imgs.txt results --dpi 70 --psm 8 -c tessedit_char_whitelist=0123456789 tsv"
